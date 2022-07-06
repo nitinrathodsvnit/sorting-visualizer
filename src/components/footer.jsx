e97b13fbe823b2fc2895ee8c0648f8d5f65fb4fa
nitin rathod <nitinrathod2300@gmail.com>
2022-07-06 13:55:38 +0530

    The basic app is ready with new features that will be added very soon.

---
+++ src/components/footer.jsx
@@ -0,0 +1,19 @@
+// import React from "react";
+// import { FaLinkedin } from "react-icons/fa";
+
+// const Footer = () => {
+//   return (
+//     <footer class="footer bg-dark py-3 mt-auto">
+//       <div class="container text-center">
+//         <span class="text-muted">
+//           Made with Love by &copy;Nitin Rathod{" "}
+//           <a href="https://www.linkedin.com/in/nitin-rathod-23/">
+//             <FaLinkedin />
+//           </a>
+//         </span>
+//       </div>
+//     </footer>
+//   );
+// };
+
+// export default Footer;
